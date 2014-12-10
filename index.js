9c7bf1bb80cf44060637721ff331a3d1dd3140f7
Jonathan Ong <jonathanrichardong@gmail.com>
2014-12-10 13:18:03 -0800

    return stack traces when not in production

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 
 var statuses = require('statuses');
 
+var production = process.env.NODE_ENV === 'production';
+
 module.exports = function () {
   return function apiErrorHandler(err, req, res, next) {
     var status = err.status || err.statusCode || 500;
     if (status < 400) status = 500;
     res.statusCode = status;
 
+    var body = {
+      status: status
+    };
+
+    // show the stacktrace when not in production
+    // TODO: make this an option
+    if (!production) body.stack = err.stack;
+
     // internal server errors
     if (status >= 500) {
       console.error(err.stack);
-
-      res.json({
-        message: statuses[status],
-        status: status,
-      });
+      body.message = statuses[status];
+      res.json(body);
       return;
     }
 
     // client errors
-    var body = {
-      message: err.message,
-      status: status,
-    };
+    body.message = err.message;
 
     if (err.code) body.code = err.code;
     if (err.name) body.name = err.name;
